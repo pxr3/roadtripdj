283b8ba5e809082f7b5b4bb5e44585b211292b75
Paul Robinson <pxr@Pauls-iMac.local>
2015-05-10 11:33:47 -0700

    Initial Commit

---
+++ RoadTripDJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RoadTripDJ
+//
+//  Created by Paul Robinson on 5/10/15.
+//  Copyright (c) 2015 Paul Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RoadTripDJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RoadTripDJ
+//
+//  Created by Paul Robinson on 5/10/15.
+//  Copyright (c) 2015 Paul Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
